258189f74e97d926e1664b7194c6aebee1a9451a
abhivora1 <31932132+abhivora1@users.noreply.github.com>
2017-12-02 00:21:28 -0500

    Add files via upload

---
+++ random_forest.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 22:29:43 2017
+
+@author: abhivora
+"""
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+import random
+import numpy as np
+
+import pandas as pd
+
+df = pd.read_csv('nike.csv', usecols=['close', 'open', 'high', 'low','score'])
+
+validate_df = pd.read_csv('Validate.csv', usecols=['open', 'high', 'low','score', 'close'])
+validate_x = validate_df[['open', 'high', 'low','score']]
+validate_y = validate_df['close']
+
+train_x, test_x, train_y, test_y = train_test_split(df[['open', 'high', 'low','score']], df['close'], train_size=0.75)
+
+clf = RandomForestRegressor()
+clf.fit(train_x, train_y)
+
+predictions = clf.predict(validate_df[['open', 'high', 'low','score']])
+
+#print("Train Accuracy :: ", accuracy_score(train_y, clf.predict(train_x)))
+#print("Test Accuracy  :: ", accuracy_score(test_y, predictions))
+#print("Confusion matrix ", confusion_matrix(test_y, predictions))
+
+print(predictions)
+print(validate_y)
+
+print(clf.score(train_x, train_y))
+print(clf.score(test_x, test_y))
+print(clf.score(validate_x, validate_y))
+
+
+predicted_values = clf.predict(validate_x)
+validate_df['predicted_close'] = predicted_values
+validate_df.to_csv('Output.csv', index=False)
+
